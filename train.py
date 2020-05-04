988283a560070ceb67f6bf9d26c435e7290aed60
Anton Makarenko <makarenko.antonf2c@gmail.com>
2020-05-04 19:51:40 +0300

    add new models

--- train.py
+++ train.py
@@ -1,12 +1,14 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 df = pd.read_csv('echocardiogram.data', sep=',', header=None)
 
 for i in list(df):
@@ -27,19 +29,23 @@ y = df[list(df)[-1]]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=42)
 
-model1 = GaussianNB()
-model1.fit(X_train, y_train)
+std = StandardScaler()
+model1 = SVC()
+model1.fit(std.fit_transform(X_train), y_train)
 
 print(accuracy_score(model1.predict(X_test), y_test))
 
-model2 = KNeighborsClassifier()
+model2 = DecisionTreeClassifier()
 model2.fit(X_train, y_train)
 
 print(accuracy_score(model2.predict(X_test), y_test))
 
-std = StandardScaler()
-X_train = std.fit_transform(X_train)
-model3 = LogisticRegression()
+model3 = GradientBoostingClassifier()
 model3.fit(X_train, y_train)
 
 print(accuracy_score(model3.predict(std.transform(X_test)), y_test))
+
+model4 = RandomForestClassifier()
+model4.fit(X_train, y_train)
+
+print(accuracy_score(model4.predict(std.transform(X_test)), y_test))
